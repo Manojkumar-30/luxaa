0fdbe0824b4c026ecb35f113ef17990ef7b70af5
Manoj Kumar <94695167+Mkvelan@users.noreply.github.com>
2023-02-21 20:20:33 +0530

    inital

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ const farDist = 10000;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	nearDist,
-	farDist
+    75,
+    window.innerWidth / window.innerHeight,
+    nearDist,
+    farDist
 );
 camera.position.x = farDist * -3;
 camera.position.z = 500;
@@ -23,23 +23,23 @@ const geometry = new THREE.BoxBufferGeometry(cubeSize, cubeSize, cubeSize); // B
 const material = new THREE.MeshNormalMaterial(); // Maps the normal vectors to RGB colors
 const group = new THREE.Group();
 for (let i = 0; i < 550; i++) {
-	const mesh = new THREE.Mesh(geometry, material);
-	const dist = farDist / 3;
-	const distDouble = dist * 2;
-	const tau = 2 * Math.PI; // One turn
+    const mesh = new THREE.Mesh(geometry, material);
+    const dist = farDist / 3;
+    const distDouble = dist * 2;
+    const tau = 2 * Math.PI; // One turn
 
-	mesh.position.x = Math.random() * distDouble - dist;
-	mesh.position.y = Math.random() * distDouble - dist;
-	mesh.position.z = Math.random() * distDouble - dist;
-	mesh.rotation.x = Math.random() * tau;
-	mesh.rotation.y = Math.random() * tau;
-	mesh.rotation.z = Math.random() * tau;
+    mesh.position.x = Math.random() * distDouble - dist;
+    mesh.position.y = Math.random() * distDouble - dist;
+    mesh.position.z = Math.random() * distDouble - dist;
+    mesh.rotation.x = Math.random() * tau;
+    mesh.rotation.y = Math.random() * tau;
+    mesh.rotation.z = Math.random() * tau;
 
-	// Manually control when 3D transformations recalculation occurs for better performance
-	mesh.matrixAutoUpdate = false;
-	mesh.updateMatrix();
+    // Manually control when 3D transformations recalculation occurs for better performance
+    mesh.matrixAutoUpdate = false;
+    mesh.updateMatrix();
 
-	group.add(mesh);
+    group.add(mesh);
 }
 scene.add(group);
 
@@ -47,72 +47,72 @@ scene.add(group);
 const loader = new THREE.FontLoader();
 const textMesh = new THREE.Mesh();
 const createTypo = font => {
-	// const word = "LUXAA";
-	const typoProperties = {
-		font: font,
-		size: cubeSize,
-		height: cubeSize / 5,
-		curveSegments: 12,
-		bevelEnabled: true,
-		bevelThickness: 10,
-		bevelSize: 6,
-		bevelOffset: 1,
-		bevelSegments: 8
-	};
-	const text = new THREE.TextGeometry(word, typoProperties);
-	textMesh.geometry = text;
-	textMesh.material = material;
-	textMesh.position.x = cubeSize * -2;
-	textMesh.position.z = cubeSize * -1;
-	scene.add(textMesh);
+    // const word = "LUXAA";
+    const typoProperties = {
+        font: font,
+        size: cubeSize,
+        height: cubeSize / 5,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 10,
+        bevelSize: 6,
+        bevelOffset: 1,
+        bevelSegments: 8
+    };
+    const text = new THREE.TextGeometry(word, typoProperties);
+    textMesh.geometry = text;
+    textMesh.material = material;
+    textMesh.position.x = cubeSize * -2;
+    textMesh.position.z = cubeSize * -1;
+    scene.add(textMesh);
 };
 loader.load(
-	"https://threejs.org/examples/fonts/helvetiker_regular.typeface.json",
-	createTypo
+    "https://threejs.org/examples/fonts/helvetiker_regular.typeface.json",
+    createTypo
 );
 
 // CREATE PART OF THE MOUSE/TOUCH OVER EFFECT
 let mouseX = 0;
 let mouseY = 0;
 const mouseFX = {
-	windowHalfX: window.innerWidth / 2,
-	windowHalfY: window.innerHeight / 2,
-	coordinates: function(coordX, coordY) {
-		mouseX = (coordX - mouseFX.windowHalfX) * 10;
-		mouseY = (coordY - mouseFX.windowHalfY) * 10;
-	},
-	onMouseMove: function(e) {
-		mouseFX.coordinates(e.clientX, e.clientY);
-	},
-	onTouchMove: function(e) {
-		mouseFX.coordinates(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
-	}
+    windowHalfX: window.innerWidth / 2,
+    windowHalfY: window.innerHeight / 2,
+    coordinates: function(coordX, coordY) {
+        mouseX = (coordX - mouseFX.windowHalfX) * 10;
+        mouseY = (coordY - mouseFX.windowHalfY) * 10;
+    },
+    onMouseMove: function(e) {
+        mouseFX.coordinates(e.clientX, e.clientY);
+    },
+    onTouchMove: function(e) {
+        mouseFX.coordinates(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
+    }
 };
 document.addEventListener("mousemove", mouseFX.onMouseMove, false);
 document.addEventListener("touchmove", mouseFX.onTouchMove, false);
 
 // RENDER 3D GRAPHIC
 const render = () => {
-	requestAnimationFrame(render);
+    requestAnimationFrame(render);
 
-	// Camera animation
-	// Works with onMouseMove and onTouchMove functions
-	camera.position.x += (mouseX - camera.position.x) * 0.05;
-	camera.position.y += (mouseY * -1 - camera.position.y) * 0.05;
-	camera.lookAt(scene.position); // Rotates the object to face a point in world space
+    // Camera animation
+    // Works with onMouseMove and onTouchMove functions
+    camera.position.x += (mouseX - camera.position.x) * 0.05;
+    camera.position.y += (mouseY * -1 - camera.position.y) * 0.05;
+    camera.lookAt(scene.position); // Rotates the object to face a point in world space
 
-	const t = Date.now() * 0.001;
-	const rx = Math.sin(t * 0.7) * 0.5;
-	const ry = Math.sin(t * 0.3) * 0.5;
-	const rz = Math.sin(t * 0.2) * 0.5;
-	group.rotation.x = rx;
-	group.rotation.y = ry;
-	group.rotation.z = rz;
-	textMesh.rotation.x = rx;
-	textMesh.rotation.y = ry;
-	textMesh.rotation.z = rx; // Happy accident :) 
+    const t = Date.now() * 0.001;
+    const rx = Math.sin(t * 0.7) * 0.5;
+    const ry = Math.sin(t * 0.3) * 0.5;
+    const rz = Math.sin(t * 0.2) * 0.5;
+    group.rotation.x = rx;
+    group.rotation.y = ry;
+    group.rotation.z = rz;
+    textMesh.rotation.x = rx;
+    textMesh.rotation.y = ry;
+    textMesh.rotation.z = rx; // Happy accident :) 
 
-	renderer.render(scene, camera);
+    renderer.render(scene, camera);
 };
 render();
 
@@ -135,9 +135,9 @@ document.addEventListener("mousemove", function(event) {
   img.style.transform = "translate(" + xPos * 20 + "px, " + yPos * 20 + "px) rotate(" + xPos * 20 + "deg)";
 });
 
-
+// Scrroll Zoom animation ---
 const banner = document.querySelector('.landing');
 window.addEventListener('scroll', () => {
   const scrollPosition = window.pageYOffset;
-  banner.style.transform = `translateY(-${scrollPosition}px) scale(${1 + scrollPosition/400})`;
+  banner.style.transform = `translateY(-${scrollPosition}px) scale(${1 + scrollPosition/350})`;
 });
